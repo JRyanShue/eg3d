1fc47413ac499c2233a30c34769d3c1f738d8fc4
JRyanShue <ryanshue.design@gmail.com>
2022-07-23 17:56:39 -0700

    Add planes argument to generator

--- eg3d/training/triplane.py
+++ eg3d/training/triplane.py
@@ -50,7 +50,7 @@ class TriPlaneGenerator(torch.nn.Module):
                 c = torch.zeros_like(c)
         return self.backbone.mapping(z, c * self.rendering_kwargs.get('c_scale', 0), truncation_psi=truncation_psi, truncation_cutoff=truncation_cutoff, update_emas=update_emas)
 
-    def synthesis(self, ws, c, neural_rendering_resolution=None, update_emas=False, cache_backbone=False, use_cached_backbone=False, **synthesis_kwargs):
+    def synthesis(self, ws, c, planes=None, neural_rendering_resolution=None, update_emas=False, cache_backbone=False, use_cached_backbone=False, **synthesis_kwargs):
         cam2world_matrix = c[:, :16].view(-1, 4, 4)
         intrinsics = c[:, 16:25].view(-1, 3, 3)
 
@@ -64,12 +64,14 @@ class TriPlaneGenerator(torch.nn.Module):
 
         # Create triplanes by running StyleGAN backbone
         N, M, _ = ray_origins.shape
-        if use_cached_backbone and self._last_planes is not None:
-            planes = self._last_planes
-        else:
-            planes = self.backbone.synthesis(ws, update_emas=update_emas, **synthesis_kwargs)
-        if cache_backbone:
-            self._last_planes = planes
+
+        if not planes:
+            if use_cached_backbone and self._last_planes is not None:
+                planes = self._last_planes
+            else:
+                planes = self.backbone.synthesis(ws, update_emas=update_emas, **synthesis_kwargs)
+            if cache_backbone:
+                self._last_planes = planes
 
         # Reshape output into three 32-channel planes
         planes = planes.view(len(planes), 3, 32, planes.shape[-2], planes.shape[-1])
@@ -88,10 +90,11 @@ class TriPlaneGenerator(torch.nn.Module):
 
         return {'image': sr_image, 'image_raw': rgb_image, 'image_depth': depth_image}
     
-    def sample(self, coordinates, directions, z, c, truncation_psi=1, truncation_cutoff=None, update_emas=False, **synthesis_kwargs):
+    def sample(self, coordinates, directions, z, c, planes=None, truncation_psi=1, truncation_cutoff=None, update_emas=False, **synthesis_kwargs):
         # Compute RGB features, density for arbitrary 3D coordinates. Mostly used for extracting shapes. 
-        ws = self.mapping(z, c, truncation_psi=truncation_psi, truncation_cutoff=truncation_cutoff, update_emas=update_emas)
-        planes = self.backbone.synthesis(ws, update_emas=update_emas, **synthesis_kwargs)
+        if not planes:
+            ws = self.mapping(z, c, truncation_psi=truncation_psi, truncation_cutoff=truncation_cutoff, update_emas=update_emas)
+            planes = self.backbone.synthesis(ws, update_emas=update_emas, **synthesis_kwargs)
         planes = planes.view(len(planes), 3, 32, planes.shape[-2], planes.shape[-1])
         return self.renderer.run_model(planes, self.decoder, coordinates, directions, self.rendering_kwargs)
 
